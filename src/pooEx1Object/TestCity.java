72422ec3dd6f4ea8a7bafd42cbbe9f44a6342d92
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-11-12 10:59:35 +0100

    Creation de la classe TestCity

---
+++ src/pooEx1Object/TestCity.java
@@ -0,0 +1,15 @@
+package pooEx1Object;
+
+public class TestCity {
+    
+    public static void main(String[] args) {
+        
+        City toulouse = new City("Toulouse", "France", 450000);
+        City paris = new City("Paris", "France", 2000000);
+        City londres = new City("Londres", "Angleterre", 1000000);
+        
+        System.out.println(toulouse.getCityName() + " " + toulouse.getCountry() + " " + toulouse.getNumberOfResidents());
+        System.out.println(paris.getCityName() + " " + paris.getCountry() + " " + paris.getNumberOfResidents());
+        System.out.println(londres.getCityName() + " " + londres.getCountry() + " " + londres.getNumberOfResidents());
+    }
+}
